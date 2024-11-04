7f7708780936df3c354f1cb97339836744f5ef92
VenkatYuvaRaaj012 <VenkatYuvaRaaj012@gmail.com>
2024-11-03 20:39:36 -0500

    My name

---
+++ WebApplication2/Controllers/MyNameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyNameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            return Ok(new { Name = "My name is Venkat YuvaRaj " });
+        }
+    }
+}
